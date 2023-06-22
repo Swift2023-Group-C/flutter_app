d27b64f1169be1ff0f82a9beacaaf013211840d2
tigertora7571 <tigertora7571@gmail.com>
2023-06-22 16:24:07 +0900

    [add] map_grid.dart

---
+++ lib/screens/map_grid.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MapGridScreen extends StatefulWidget {
+  const MapGridScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MapGridScreen> createState() => _MapGridScreenState();
+}
+
+class _MapGridScreenState extends State<MapGridScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
