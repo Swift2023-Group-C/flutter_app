feda620cf2c8fcd62cdf71f0f3104f1c63656294
hikaru-0602 <beretta0602@gmail.com>
2023-09-20 17:15:09 +0900

    [fix] db

--- lib/screens/kamoku.dart
+++ lib/screens/kamoku.dart
@@ -16,7 +16,7 @@ Future<List<Map<String, dynamic>>> fetchRecords() async {
 
   Database database = await openDatabase(copiedDbPath);
   List<Map<String, dynamic>> records =
-      await database.rawQuery('SELECT LessonId,授業名 FROM database2');
+      await database.rawQuery('SELECT LessonId,授業名 FROM test');
   return records;
 }
 
@@ -25,16 +25,13 @@ Future<Map<String, dynamic>> fetchDetails(String className) async {
   String copiedDbPath = join(dbPath, 'コピーされたデータベース.db');
 
   Database database = await openDatabase(copiedDbPath);
-  List<Map<String, dynamic>> details = await database
-      .query('database2', where: '授業名 = ?', whereArgs: [className]);
-
-  //List<Map<String, dynamic>> info = await database
-  //  .query('database2', where: 'LessonId = ?', whereArgs: [className]);
+  List<Map<String, dynamic>> details =
+      await database.query('test', where: '授業名 = ?', whereArgs: [className]);
 
   if (details.isNotEmpty) {
     return details.first;
   } else {
-    return {}; // エラーハンドリングを行うことを検討してください
+    return {};
   }
 }
 
@@ -95,52 +92,67 @@ class DetailScreen extends StatelessWidget {
       body: FutureBuilder<Map<String, dynamic>>(
         future: fetchDetails(className),
         builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return CircularProgressIndicator();
-          } else if (snapshot.hasError) {
-            return Text('Error: ${snapshot.error}');
-          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return Text('詳細情報が見つかりません');
-          } else {
-            Map<String, dynamic> details = snapshot.data!;
-            // 詳細情報を表示するウィジェットを構築することができます
-            // 例えば、Text ウィジェットを使用して詳細情報を表示することができます
-            return ListView.builder(
-              itemCount: 1, // リストアイテムは1つのみ
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text('LessonId: ${details['LessonId']}'),
-                  subtitle: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('授業名: ${details['授業名']}'),
-                      Text('Course: ${details['Course']}'),
-                      Text('配当年次: ${details['配当年次']}'),
-                      Text('開講時期: ${details['開講時期']}'),
-                      Text('単位数: ${details['単位数']}'),
-                      Text('担当教員名: ${details['担当教員名']}'),
-                      Text('実務家教員区分: ${details['実務家教員区分']}'),
-                      Text('授業形態: ${details['授業形態']}'),
-                      Text('授業の概要: ${details['授業の概要']}'),
-                      Text('授業の到達目標: ${details['授業の到達目標']}'),
-                      Text('提出課題等: ${details['提出課題等']}'),
-                      Text('成績の評価方法・基準: ${details['成績の評価方法・基準']}'),
-                      Text('テキスト: ${details['テキスト']}'),
-                      Text('参考書: ${details['参考書']}'),
-                      Text('履修条件: ${details['履修条件']}'),
-                      Text('事前学習: ${details['事前学習']}'),
-                      Text('事後学習: ${details['事後学習']}'),
-                      Text('履修上の留意点: ${details['履修上の留意点']}'),
-                      Text('キーワード/keyword: ${details['キーワード/keyword']}'),
-                      Text('対象コース・領域: ${details['対象コース・領域']}'),
-                      Text('科目群・科目区分: ${details['科目群・科目区分']}'),
-                      // 他のカラムも同様に表示できます
-                    ],
-                  ),
-                );
-              },
-            );
-          }
+          Map<String, dynamic> details = snapshot.data!;
+          // 詳細情報を表示するウィジェットを構築することができます
+          // 例えば、Text ウィジェットを使用して詳細情報を表示することができます
+          return ListView.builder(
+            itemCount: 1, // リストアイテムは1つのみ
+            itemBuilder: (context, index) {
+              return ListTile(
+                //title: Text('LessonId: ${details['LessonId']}'),
+                title: Text(
+                  '詳細情報',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                ),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('授業名: ${details['授業名']}'),
+                    Text('Course: ${details['Course']}'),
+                    Text('配当年次: ${details['配当年次']}'),
+                    Text('開講時期: ${details['開講時期']}'),
+                    Text('単位数: ${details['単位数']}'),
+                    Text('担当教員名: ${details['担当教員名']}'),
+                    Text('実務家教員区分: ${details['実務家教員区分']}'),
+                    Text('授業形態: ${details['授業形態']}'),
+                    Text('授業の概要: ${details['授業の概要']}'),
+                    Text('授業の到達目標: ${details['授業の到達目標']}'),
+                    Text('提出課題等: ${details['提出課題等']}'),
+                    Text('成績の評価方法・基準: ${details['成績の評価方法・基準']}'),
+                    Text('テキスト: ${details['テキスト']}'),
+                    Text('参考書: ${details['参考書']}'),
+                    Text('履修条件: ${details['履修条件']}'),
+                    Text('事前学習: ${details['事前学習']}'),
+                    Text('事後学習: ${details['事後学習']}'),
+                    Text('履修上の留意点: ${details['履修上の留意点']}'),
+                    Text('キーワード/keyword: ${details['キーワード/keyword']}'),
+                    Text('対象コース・領域: ${details['対象コース・領域']}'),
+                    Text('科目群・科目区分: ${details['科目群・科目区分']}'),
+                    Text('授業・試験の形式: ${details['授業・試験の形式']}'),
+                    Text('教授言語: ${details['教授言語']}'),
+                    Text('DSOP対象科目: ${details['DSOP対象科目']}'),
+                    Text('(E)Course outline: ${details['(E)Course outline']}'),
+                    Text(
+                        '(E)Course Objectives: ${details['(E)Course Objectives']}'),
+                    Text(
+                        '(E)Course Schedule: ${details['(E)Course Schedule']}'),
+                    Text(
+                        '(E)Prior/Post Assignment: ${details['(E)Prior/Post Assignment']}'),
+                    Text('(E)Assessment: ${details['(E)Assessment']}'),
+                    Text('(E)Textbooks: ${details['(E)Textbooks']}'),
+                    Text(
+                        '(E)Language of Instruction: ${details['(E)Language of Instruction']}'),
+                    Text('(E)Note: ${details['(E)Note']}'),
+                    Text(
+                        '(E)Requirements for registration: ${details['(E)Requirements for registration']}'),
+                    Text(
+                        '(E)Type of class and exam: ${details['(E)Type of class and exam']}'),
+                    // 他のカラムも同様に表示できます
+                  ],
+                ),
+              );
+            },
+          );
         },
       ),
     );
