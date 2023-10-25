8c7bdde336fa953eaeb8980cd25051aa4b6c1612
tigertora7571 <tigertora7571@gmail.com>
2023-10-25 16:05:32 +0900

    [fix] map 色つかない問題

--- lib/screens/map_grid.dart
+++ lib/screens/map_grid.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/components/widgets/progress_indicator.dart';
 import 'package:flutter_app/screens/map.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:flutter_app/components/widgets/map.dart';
 
 import 'package:flutter_app/repository/find_rooms_in_use.dart';
-import 'package:flutter_app/repository/download_file_from_firebase.dart';
 import 'package:flutter_app/repository/read_json_file.dart';
 
 class MapGridScreen extends StatefulWidget {
@@ -17,28 +17,39 @@ class MapGridScreen extends StatefulWidget {
 
 class _MapGridScreenState extends State<MapGridScreen> {
   final List<String> gridMapsList = ["1", "2", "3", "4", "5", "R1", "R2"];
+  Future<int?>? _future;
   @override
   Widget build(BuildContext context) {
     return Consumer(builder: (context, ref, child) {
       final mapPage = ref.watch(mapPageProvider);
-      return StaggeredGrid.count(
-        crossAxisCount: 48,
-        children: [
-          ...GridMaps.mapTileListMap[gridMapsList[mapPage]]!.map(
-            (e) {
-              return StaggeredGridTile.count(
-                  crossAxisCellCount: e.width,
-                  mainAxisCellCount: e.height,
-                  child: e);
-            },
-          )
-        ],
+      return FutureBuilder(
+        future: _future,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return StaggeredGrid.count(
+              crossAxisCount: 48,
+              children: [
+                ...GridMaps.mapTileListMap[gridMapsList[mapPage]]!.map(
+                  (e) {
+                    return StaggeredGridTile.count(
+                        crossAxisCellCount: e.width,
+                        mainAxisCellCount: e.height,
+                        child: e);
+                  },
+                )
+              ],
+            );
+          } else {
+            return Center(
+              child: createProgressIndicator(),
+            );
+          }
+        },
       );
     });
   }
 
-  @override
-  void initState() {
+  Future<int?> initUsingColor() async {
     final Map<String, List<String>> classroomNoFloorMap = {
       "1": ["4", "5"],
       "2": ["3"],
@@ -62,23 +73,22 @@ class _MapGridScreenState extends State<MapGridScreen> {
       "50": ["1"],
       "51": ["3"],
     };
-    super.initState();
-    // アプリ起動時に一度だけ実行される
-    // initState内で非同期処理を行うための方法
-    WidgetsBinding.instance.addPostFrameCallback((_) async {
-      String scheduleFilePath = 'map/oneweek_schedule.json';
-      // Firebaseからファイルをダウンロード
-      await downloadFileFromFirebase(scheduleFilePath);
 
-      // ダウンロードしたファイルの中身を読み取る
-      try {
-        String fileContent = await readJsonFile(scheduleFilePath);
-        Map<String, DateTime> resourceIds = findRoomsInUse(fileContent);
+    String scheduleFilePath = 'map/oneweek_schedule.json';
+    Map<String, DateTime>? resourceIds;
+    try {
+      String fileContent = await readJsonFile(scheduleFilePath);
+      resourceIds = findRoomsInUse(fileContent);
+    } catch (e) {
+      print(e);
+    }
 
-        if (resourceIds.isNotEmpty) {
-          resourceIds.forEach((String resourceId, DateTime useEndTime) {
-            print(resourceId);
-            if (classroomNoFloorMap.containsKey(resourceId)) {
+    if (resourceIds != null) {
+      if (resourceIds.isNotEmpty) {
+        resourceIds.forEach((String resourceId, DateTime useEndTime) {
+          print(resourceId);
+          if (classroomNoFloorMap.containsKey(resourceId)) {
+            setState(() {
               for (var floor in classroomNoFloorMap[resourceId]!) {
                 final tileIndex = GridMaps.mapTileListMap[floor]!
                     .indexWhere((tile) => tile.classroomNo == resourceId);
@@ -92,14 +102,20 @@ class _MapGridScreenState extends State<MapGridScreen> {
                       .setUseEndTime(useEndTime);
                 }
               }
-            }
-          });
-        } else {
-          print("No ResourceId exists for the current time.");
-        }
-      } catch (e) {
-        print(e);
+            });
+          }
+        });
+        return 1;
       }
-    });
+    } else {
+      print("No ResourceId exists for the current time.");
+    }
+    return null;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _future = initUsingColor();
   }
 }
