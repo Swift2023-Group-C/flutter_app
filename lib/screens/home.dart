e3822022ee4fd21cd51dc7ff30a290caae7cc174
unknown <msyaaa456910@gmail.com>
2023-10-27 22:15:15 +0900

    ホーム画面にガイド的なやつ追加してみた

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/screens/kamoku.dart';
 import 'package:flutter_app/screens/setting.dart';
+import 'package:flutter_app/screens/app_usage_guide.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -8,40 +10,86 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-          child: Column(
-        children: [
-          const Text('ホーム', style: TextStyle(fontSize: 32.0)),
-          IconButton(
-            icon: const Icon(
-              Icons.settings,
-              size: 40,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //const Text('ホーム', style: TextStyle(fontSize: 32.0)),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  PageRouteBuilder(
+                    pageBuilder: (context, animation, secondaryAnimation) {
+                      return const AppGuideScreen();
+                    },
+                    transitionsBuilder:
+                        (context, animation, secondaryAnimation, child) {
+                      const Offset begin = Offset(0.0, 1.0);
+                      const Offset end = Offset.zero;
+                      final Animatable<Offset> tween =
+                          Tween(begin: begin, end: end)
+                              .chain(CurveTween(curve: Curves.easeInOut));
+                      final Animation<Offset> offsetAnimation =
+                          animation.drive(tween);
+                      return SlideTransition(
+                        position: offsetAnimation,
+                        child: child,
+                      );
+                    },
+                  ),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.amber,
+                fixedSize: const Size(250, 100),
+              ),
+              child: const Text('このアプリの使い方'),
             ),
-            onPressed: () {
-              Navigator.of(context).push(
-                PageRouteBuilder(
-                  pageBuilder: (context, animation, secondaryAnimation) {
-                    return const SettingScreen();
-                  },
-                  transitionsBuilder:
-                      (context, animation, secondaryAnimation, child) {
-                    const Offset begin = Offset(0.0, 1.0);
-                    const Offset end = Offset.zero;
-                    final Animatable<Offset> tween =
-                        Tween(begin: begin, end: end)
-                            .chain(CurveTween(curve: Curves.easeInOut));
-                    final Animation<Offset> offsetAnimation =
-                        animation.drive(tween);
-                    return SlideTransition(
-                      position: offsetAnimation,
-                      child: child,
-                    );
-                  },
-                ),
-              );
-            },
-          ),
-        ],
-      )),
+            const Spacer(),
+            ElevatedButton.icon(
+              onPressed: () {
+                Navigator.of(context).push(
+                  PageRouteBuilder(
+                    pageBuilder: (context, animation, secondaryAnimation) {
+                      return const SettingScreen();
+                    },
+                    transitionsBuilder:
+                        (context, animation, secondaryAnimation, child) {
+                      const Offset begin = Offset(0.0, 1.0);
+                      const Offset end = Offset.zero;
+                      final Animatable<Offset> tween =
+                          Tween(begin: begin, end: end)
+                              .chain(CurveTween(curve: Curves.easeInOut));
+                      final Animation<Offset> offsetAnimation =
+                          animation.drive(tween);
+                      return SlideTransition(
+                        position: offsetAnimation,
+                        child: child,
+                      );
+                    },
+                  ),
+                );
+              },
+              icon: const Icon(Icons.settings),
+              label: const Text(
+                '設定',
+                style: TextStyle(fontSize: 20),
+              ),
+              style: ElevatedButton.styleFrom(
+                fixedSize: const Size(100, 50),
+              ),
+            ),
+            const Spacer(),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green,
+                fixedSize: const Size(250, 100),
+              ),
+              child: const Text('過去問は科目検索画面から'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
