dd479dbb250fd6409cec4b9f600f6df74acdc509
tigertora7571 <tigertora7571@gmail.com>
2023-10-29 12:21:20 +0900

    [fix] 削除log

--- lib/repository/find_rooms_in_use.dart
+++ lib/repository/find_rooms_in_use.dart
@@ -3,7 +3,6 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 
 Map<String, DateTime> findRoomsInUse(String jsonString) {
-  debugPrint("Start findRoomsInUse");
   var decodedData = jsonDecode(jsonString);
 
   // JSONデータがリストでない場合はエラー
@@ -21,11 +20,9 @@ Map<String, DateTime> findRoomsInUse(String jsonString) {
       DateTime startTime =
           DateTime.parse(item['start']).add(const Duration(minutes: -10));
       DateTime endTime = DateTime.parse(item['end']);
-      debugPrint("mid findRoomsInUse");
 
       //現在時刻が開始時刻と終了時刻の間であればresourceIdを取得
       if (now.isAfter(startTime) && now.isBefore(endTime)) {
-        debugPrint("mid2 findRoomsInUse");
         if (resourceIds.containsKey(item['resourceId'])) {
           debugPrint(item['resourceId']);
           if (resourceIds[item['resourceId']]!.isBefore(endTime)) {
