c10d0fd5c8878235742646f1023427382f6f9d05
tigertora7571 <tigertora7571@gmail.com>
2023-11-15 23:49:12 +0900

    [release] 0.1.1

--- lib/repository/download_file_from_firebase.dart
+++ lib/repository/download_file_from_firebase.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-import 'package:flutter/material.dart';
 import 'package:flutter_app/repository/get_application_path.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 
