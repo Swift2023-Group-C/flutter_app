5c476ea756fc73d0c59584b8b696c413f8611457
hikaru-0602 <beretta0602@gmail.com>
2023-11-20 00:22:25 +0900

    時間割リスト開講時期で分けた

--- lib/screens/personal_time_table.dart
+++ lib/screens/personal_time_table.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/components/db_config.dart';
 import 'package:flutter_app/components/setting_user_info.dart';
 import 'dart:convert';
+import 'package:flutter_app/components/color_fun.dart';
+import 'package:sqflite/sqflite.dart';
 
 class PersonalTimeTableScreen extends StatefulWidget {
-  PersonalTimeTableScreen({Key? key}) : super(key: key);
+  const PersonalTimeTableScreen({Key? key}) : super(key: key);
 
   @override
   State<PersonalTimeTableScreen> createState() =>
@@ -12,6 +15,9 @@ class PersonalTimeTableScreen extends StatefulWidget {
 
 class _PersonalTimeTableScreenState extends State<PersonalTimeTableScreen> {
   List<int> personalTimeTableList = [];
+  late List<Map<String, dynamic>> records = [];
+  final PageController _pageController = PageController();
+  int _currentPageIndex = 0;
 
   Future<void> loadPersonalTimeTableList() async {
     final jsonString = await UserPreferences.getFinishList();
@@ -26,29 +32,149 @@ class _PersonalTimeTableScreenState extends State<PersonalTimeTableScreen> {
     await UserPreferences.setFinishList(json.encode(personalTimeTableList));
   }
 
+  Future<List<Map<String, dynamic>>> fetchRecords() async {
+    Database database = await openDatabase(SyllabusDBConfig.dbPath);
+
+    String whereClause = personalTimeTableList.map((id) => '?').join(', ');
+    List<Map<String, dynamic>> records = await database.query(
+      'week_period',
+      where: 'lessonID IN ($whereClause)',
+      whereArgs: personalTimeTableList,
+    );
+    return records;
+  }
+
+  Widget seasonTimeTableList(
+      int seasonnumber, List<Map<String, dynamic>> records) {
+    List<Map<String, dynamic>> seasonList = records.where((record) {
+      return record['開講時期'] == seasonnumber;
+    }).toList();
+    return ListView.builder(
+      itemCount: seasonList.length,
+      itemBuilder: (context, index) {
+        var record = seasonList[index];
+        return ListTile(
+          title: Text(record['授業名'] ?? '不明な授業'),
+          /* よくわからなくなったので保留
+          leading: IconButton(
+              onPressed: () async {
+                setState(() {
+                  personalTimeTableList
+                      .removeWhere((item) => item == record['LessonId']);
+                });
+                await savePersonalTimeTableList();
+                var updatedRecords = await fetchRecords();
+                setState(() {
+                  records = updatedRecords;
+                });
+                print(personalTimeTableList);
+              },
+              icon: const Icon(Icons.playlist_remove)),*/
+        );
+      },
+    );
+  }
+
   @override
   void initState() {
     super.initState();
     loadPersonalTimeTableList();
+    fetchRecords().then((value) {
+      setState(() {
+        records = value;
+      });
+    });
   }
 
   @override
   Widget build(BuildContext context) {
+    final deviceWidth = MediaQuery.of(context).size.width;
+    final deviceHeight = MediaQuery.of(context).size.height;
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('時間割'),
-        ),
-        body: ListView.builder(
-            itemCount: personalTimeTableList.length,
-            itemBuilder: (context, index) {
-              return ListTile(
-                title: Text(
-                  personalTimeTableList[index].toString(),
+      appBar: AppBar(
+        title: const Text('時間割'),
+        actions: [
+          IconButton(
+              onPressed: () {
+                //print(records);
+              },
+              icon: const Icon(Icons.abc))
+        ],
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: deviceHeight * 0.06,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: List.generate(
+                3,
+                (index) => GestureDetector(
+                  onTap: () {
+                    _pageController.animateToPage(
+                      index,
+                      duration: const Duration(milliseconds: 300),
+                      curve: Curves.ease,
+                    );
+                  },
+                  child: Container(
+                    width: deviceWidth * 0.3,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      border: Border(
+                        bottom: BorderSide(
+                          color: index == _currentPageIndex
+                              ? customFunColor.shade400
+                              : Colors.transparent, // 非選択時は透明
+                          width: deviceWidth * 0.005,
+                        ),
+                      ),
+                    ),
+                    child: Text(
+                      _getPageName(index),
+                      style: TextStyle(
+                        fontSize: deviceWidth / 25,
+                        fontWeight: FontWeight.bold,
+                        color: index == _currentPageIndex
+                            ? customFunColor.shade400
+                            : Colors.black,
+                      ),
+                    ),
+                  ),
                 ),
-                onTap: () {
-                  print(personalTimeTableList[index]);
-                },
-              );
-            }));
+              ),
+            ),
+          ),
+          Expanded(
+            child: PageView(
+              controller: _pageController,
+              onPageChanged: (index) {
+                setState(() {
+                  _currentPageIndex = index;
+                });
+              },
+              children: [
+                seasonTimeTableList(10, records),
+                seasonTimeTableList(20, records),
+                seasonTimeTableList(0, records),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  String _getPageName(int index) {
+    switch (index) {
+      case 0:
+        return '前期';
+      case 1:
+        return '後期';
+      case 2:
+        return '通年';
+      default:
+        return '';
+    }
   }
 }
