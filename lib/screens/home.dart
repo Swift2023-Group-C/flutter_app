0284168c6814d75db6aaafd69fa3d28a011d7b77
tigertora7571 <tigertora7571@gmail.com>
2023-11-28 20:37:47 +0900

    [fix] iOS google_sign_in crash problem

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -40,15 +40,18 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
     // Trigger the authentication flow
     final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
 
+    if (googleUser == null) {
+      return null;
+    }
     // Obtain the auth details from the request
-    final GoogleSignInAuthentication? googleAuth =
-        await googleUser?.authentication;
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser.authentication;
 
     try {
       // Create a new credential
       final credential = GoogleAuthProvider.credential(
-        accessToken: googleAuth?.accessToken,
-        idToken: googleAuth?.idToken,
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
       );
       // Once signed in, return the UserCredential
       return await FirebaseAuth.instance.signInWithCredential(credential);
