2a9c8b6645529a170fafca46368982a613c81251
Toranosuke Minamikawa <tigertora7571@gmail.com>
2024-11-05 16:40:35 +0900

    fix: delay

--- lib/repository/read_json_file.dart
+++ lib/repository/read_json_file.dart
@@ -9,6 +9,10 @@ Future<String> readJsonFile(String fileName) async {
   if (await file.exists()) {
     // ファイルの内容を文字列として読み込む
     String content = await file.readAsString();
+    if (content == "") {
+      await Future.delayed(const Duration(milliseconds: 100));
+      return await readJsonFile(fileName);
+    }
     //print("read success");
     return content;
   } else {
