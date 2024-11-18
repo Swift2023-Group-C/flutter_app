4163dc21988afa7588e3f12556f6d1bf3a7fac89
Toranosuke Minamikawa <tigertora7571@gmail.com>
2024-11-18 20:43:02 +0900

    fix: R2 #162

--- lib/components/s3.dart
+++ lib/components/s3.dart
@@ -1,5 +1,6 @@
 import 'package:minio/minio.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:minio/models.dart';
 
 class S3 {
   Minio? _minio;
@@ -16,10 +17,40 @@ class S3 {
     return _minio!;
   }
 
+  Stream<ListObjectsResult> listObjectsV2(
+    String bucket, {
+    String prefix = '',
+    String? startAfter,
+  }) async* {
+    MinioInvalidBucketNameError.check(bucket);
+    MinioInvalidPrefixError.check(prefix);
+    final delimiter = '';
+
+    bool? isTruncated = false;
+    String? continuationToken;
+
+    do {
+      final resp = await _minio!.listObjectsV2Query(
+        bucket,
+        prefix,
+        continuationToken,
+        delimiter,
+        null,
+        startAfter,
+      );
+      isTruncated = resp.isTruncated;
+      continuationToken = resp.nextContinuationToken;
+      yield ListObjectsResult(
+        objects: resp.contents!,
+        prefixes: resp.commonPrefixes.map((e) => e.prefix!).toList(),
+      );
+    } while (isTruncated!);
+  }
+
   Future<List<String>> getListObjectsKey({required String url}) async {
     instance.getMinio();
     List<String> returnStr = [];
-    await for (var value in _minio!.listObjectsV2('kakomon', prefix: url, recursive: true)) {
+    await for (var value in listObjectsV2('kakomon', prefix: url)) {
       for (var obj in value.objects) {
         returnStr.add(obj.key!);
       }
