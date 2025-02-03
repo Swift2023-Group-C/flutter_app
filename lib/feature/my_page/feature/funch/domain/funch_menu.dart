aa9fa06aa425f79f694cd93aff942f5194ddc3c2
Toranosuke Minamikawa <tigertora7571@gmail.com>
2025-02-03 13:12:21 +0900

    fix: add FunchOriginalMenu

--- lib/feature/my_page/feature/funch/domain/funch_menu.dart
+++ lib/feature/my_page/feature/funch/domain/funch_menu.dart
@@ -5,8 +5,10 @@ class FunchMenu {
   final int category;
   final String? size;
   final List<String> imageUrl;
+  final int energy;
 
-  FunchMenu(this.itemCode, this.name, this.price, this.category, this.size, this.imageUrl);
+  FunchMenu(
+      this.itemCode, this.name, this.price, this.category, this.size, this.imageUrl, this.energy);
 
   factory FunchMenu.fromMenuJson(Map map) {
     final itemCode = int.parse(map["item_code"]);
@@ -15,6 +17,26 @@ class FunchMenu {
     final category = int.parse(map["category_code"]);
     final size = map["size"];
     final imageUrl = (map["image_url"] as List).map((e) => e.toString()).toList();
-    return FunchMenu(itemCode, name, price, category, size, imageUrl);
+    final energy = map["nutritionalvalue"]["energy"];
+    return FunchMenu(itemCode, name, price, category, size, imageUrl, energy);
   }
+}
+
+class FunchOriginalMenu {
+  final int id;
+  final String title;
+  final int category;
+  final bool large;
+  final bool small;
+  final String image;
+
+  FunchOriginalMenu(this.id, this.title, this.category, this.large, this.small, this.image);
+}
+
+class Prices {
+  final int large;
+  final int medium;
+  final int small;
+
+  Prices(this.large, this.medium, this.small);
 }
