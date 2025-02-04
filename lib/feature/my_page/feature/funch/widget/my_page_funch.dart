6a2341ef617f3ae88c21e7df4cc0aa57f4bdb7f1
Toranosuke Minamikawa <tigertora7571@gmail.com>
2025-02-04 18:02:49 +0900

    fix: mypage widget onclick #112

--- lib/feature/my_page/feature/funch/widget/my_page_funch.dart
+++ lib/feature/my_page/feature/funch/widget/my_page_funch.dart
@@ -3,6 +3,7 @@ import 'package:dotto/components/app_color.dart';
 import 'package:dotto/components/widgets/progress_indicator.dart';
 import 'package:dotto/feature/my_page/feature/funch/controller/funch_controller.dart';
 import 'package:dotto/feature/my_page/feature/funch/domain/funch_menu.dart';
+import 'package:dotto/feature/my_page/feature/funch/funch.dart';
 import 'package:dotto/feature/my_page/feature/funch/repository/funch_repository.dart';
 import 'package:dotto/feature/my_page/feature/funch/widget/funch_price_list.dart';
 import 'package:dotto/importer.dart';
@@ -11,9 +12,9 @@ import 'package:intl/intl.dart';
 class MyPageFunch extends ConsumerWidget {
   const MyPageFunch({super.key});
 
-  Future<List<FunchCoopMenu>> _getDaysMenu(WidgetRef ref, DateTime date) async {
+  Future<List<FunchMenu>> _getDaysMenu(WidgetRef ref, DateTime date) async {
     final funchDaysMenu = await ref.watch(funchDaysMenuProvider);
-    return funchDaysMenu[date]?.menu ?? [];
+    return [...funchDaysMenu[date]?.menu ?? [], ...funchDaysMenu[date]?.originalMenu ?? []];
   }
 
   ImageProvider<Object> _getBackgroundImage(String imageUrl) {
@@ -42,105 +43,113 @@ class MyPageFunch extends ConsumerWidget {
     final borderRadius = 10.0;
     final nextDay = FunchRepository().nextDay();
     final dayString = _getDayString(nextDay);
-    return FutureBuilder(
-      future: _getDaysMenu(ref, nextDay),
-      builder: (context, snapshot) {
-        if (!snapshot.hasData) {
-          return Center(child: createProgressIndicator());
-        }
-        final menu = snapshot.data as List<FunchCoopMenu>;
-        if (menu.isEmpty) {
-          return Center(
-            child: Card(
-              color: AppColor.textWhite,
-              child: Container(
-                width: MediaQuery.of(context).size.width * 0.85,
-                padding: EdgeInsets.all(10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "$dayStringの学食",
-                      style: TextStyle(fontSize: 18),
-                    ),
-                    SizedBox(height: 10),
-                    Center(
-                      child: Text("$dayStringの学食情報はありません"),
+    return GestureDetector(
+      onTap: () => Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const FunchScreen(),
+        ),
+      ),
+      child: FutureBuilder(
+        future: _getDaysMenu(ref, nextDay),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return Center(child: createProgressIndicator());
+          }
+          final menu = snapshot.data as List<FunchMenu>;
+          if (menu.isEmpty) {
+            return Center(
+              child: Card(
+                color: AppColor.textWhite,
+                child: Container(
+                  width: MediaQuery.of(context).size.width * 0.85,
+                  padding: EdgeInsets.all(10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "$dayStringの学食",
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      SizedBox(height: 10),
+                      Center(
+                        child: Text("$dayStringの学食情報はありません"),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          }
+          return CarouselSlider(
+            items: menu.map((e) {
+              return Container(
+                margin: EdgeInsets.all(MediaQuery.of(context).size.width * 0.075),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(borderRadius),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 0,
+                      blurRadius: 1,
+                      offset: const Offset(0, 1.5),
                     ),
                   ],
                 ),
-              ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        "$dayStringの学食",
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ),
+                    Expanded(
+                      child: Image(
+                        fit: BoxFit.cover,
+                        width: double.infinity,
+                        image: _getBackgroundImage(e.imageUrl),
+                        errorBuilder: (context, error, stackTrace) {
+                          return Image.asset('assets/images/no_image.png');
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            e.name,
+                            style: const TextStyle(
+                              fontSize: 18,
+                            ),
+                          ),
+                          Divider(
+                            height: 2,
+                            color: AppColor.dividerGrey,
+                          ),
+                          SizedBox(height: 5),
+                          FunchPriceList(e, isHome: true),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            }).toList(),
+            options: CarouselOptions(
+              aspectRatio: 4 / 3,
+              autoPlay: true,
+              autoPlayInterval: const Duration(seconds: 4),
+              viewportFraction: 1,
             ),
           );
-        }
-        return CarouselSlider(
-          items: menu.map((e) {
-            return Container(
-              margin: EdgeInsets.all(MediaQuery.of(context).size.width * 0.075),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(borderRadius),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    spreadRadius: 0,
-                    blurRadius: 1,
-                    offset: const Offset(0, 1.5),
-                  ),
-                ],
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: EdgeInsets.all(10),
-                    child: Text(
-                      "$dayStringの学食",
-                      style: TextStyle(fontSize: 18),
-                    ),
-                  ),
-                  Expanded(
-                    child: Image(
-                      fit: BoxFit.cover,
-                      width: double.infinity,
-                      image: _getBackgroundImage(e.imageUrl),
-                      errorBuilder: (context, error, stackTrace) {
-                        return Image.asset('assets/images/no_image.png');
-                      },
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          e.name,
-                          style: const TextStyle(
-                            fontSize: 18,
-                          ),
-                        ),
-                        Divider(
-                          height: 2,
-                          color: AppColor.dividerGrey,
-                        ),
-                        SizedBox(height: 5),
-                        FunchPriceList(e, isHome: true),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            );
-          }).toList(),
-          options: CarouselOptions(
-            aspectRatio: 4 / 3,
-            autoPlay: true,
-            autoPlayInterval: const Duration(seconds: 4),
-            viewportFraction: 1,
-          ),
-        );
-      },
+        },
+      ),
     );
   }
 }
